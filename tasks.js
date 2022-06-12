c24e45a694054eb122afcb297a4792bb9d26c365
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-06-12 12:16:56 +0300

    task 1 solved

--- tasks.js
+++ tasks.js
@@ -1,1 +1,27 @@
-console.log('hello!');
+function findDuplicates(inputAsString){
+    
+    //first attempt at solution, maybe too bulky
+    let duplicatesAsObj = inputAsString
+    .toLowerCase()
+    .split('')
+    .reduce((accumulator, item)=>{
+        if(!accumulator.hasOwnProperty(item)){
+            accumulator[item] = 0;
+        }
+        accumulator[item]++;
+
+    }, {});
+    
+    return Object.values(duplicatesAsObj)
+    .filter(item=>item>1)
+    .length;
+    
+    //second attempt at solution
+    
+    
+    
+}
+
+function dnaStrand(inputAsString){
+
+}
